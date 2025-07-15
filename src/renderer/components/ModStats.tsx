de410c40b8a3bbed6721ac919d1665dcc6068ac6
ZickZenni <zickzenniyt@gmail.com>
2025-07-15 12:18:31 +0200

    Convert ModStats numbers into locale strings

--- src/renderer/components/ModStats.tsx
+++ src/renderer/components/ModStats.tsx
@@ -12,15 +12,15 @@ export default function ModStats(props: Props) {
         <div className="flex gap-4">
             <div className="flex gap-1 items-center">
                 <Heart className="w-4 h-4 text-red-400" />
-                <p className="text-background-500 font-medium">{mod.likes}</p>
+                <p className="text-background-500 font-medium">{mod.likes.toLocaleString()}</p>
             </div>
             <div className="flex gap-1 items-center">
                 <MessageCircle className="w-4 h-4 text-primary-400" />
-                <p className="text-background-500 font-medium">{mod.comments}</p>
+                <p className="text-background-500 font-medium">{mod.comments.toLocaleString()}</p>
             </div>
             <div className="flex gap-1 items-center">
                 <Eye className="w-4 h-4 text-secondary-200 mb-0.25" />
-                <p className="text-background-500 font-medium mt-0.25">{mod.views}</p>
+                <p className="text-background-500 font-medium mt-0.25">{mod.views.toLocaleString()}</p>
             </div>
         </div>
     )
