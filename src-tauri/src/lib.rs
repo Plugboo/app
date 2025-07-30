c13435aafbb063553efde0f8ec7d5e48e95ea70a
ZickZenni <zickzenniyt@gmail.com>
2025-07-30 13:10:33 +0200

    Add plugins: dialog, shell, opener, notification, single_instance and logging

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,47 @@
+use tauri::{App, Manager};
+use tauri_plugin_log::fern::colors::{Color, ColoredLevelConfig};
+
+pub fn init(_app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
+    log::info!("Initializing...");
+    Ok(())
+}
+
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .format(|out, message, record| {
+                    out.finish(format_args!(
+                        "[{} {}] {}",
+                        record.level(),
+                        record.target(),
+                        message
+                    ))
+                })
+                .targets([
+                    tauri_plugin_log::Target::new(tauri_plugin_log::TargetKind::Webview),
+                    tauri_plugin_log::Target::new(tauri_plugin_log::TargetKind::Stdout),
+                ])
+                .with_colors(ColoredLevelConfig {
+                    error: Color::Red,
+                    warn: Color::Yellow,
+                    debug: Color::White,
+                    info: Color::BrightGreen,
+                    trace: Color::Cyan,
+                })
+                .build(),
+        )
+        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
-        //        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }))
+        .setup(init)
         .run(tauri::generate_context!())
         .expect("Error while running tauri application");
 }
