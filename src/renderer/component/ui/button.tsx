df8f9cb3b234ab851f1bbe65747013145e75bbfd
Calvin Rohloff <zickzenniyt@gmail.com>
2026-03-26 12:34:41 +0100

    Add `Button` component and enhance game overview page visuals
    - Introduced a reusable `Button` component.
    - Enhanced `GameOverviewPage` by adding profile cards, installation status, and playtime details.

---
+++ src/renderer/component/ui/button.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { Button as BaseButton } from "@base-ui/react/button";
+
+interface Props
+{
+    children?: React.ReactNode;
+    isDisabled?: boolean;
+}
+
+export default function Button(props: Props)
+{
+    return (
+        <BaseButton
+            className="flex items-center justify-center h-10 px-3.5 m-0 outline-0 border border-gray-200 bg-gray-50 font-inherit text-base font-medium leading-6 text-gray-900 select-none hover:data-disabled:bg-gray-50 hover:bg-gray-100 active:data-disabled:bg-gray-50 active:bg-gray-200 active:shadow-[inset_0_1px_3px_rgba(0,0,0,0.1)] active:border-t-gray-300 active:data-disabled:shadow-none active:data-disabled:border-t-gray-200 focus-visible:outline-2 focus-visible:outline-blue-800 focus-visible:-outline-offset-1 data-disabled:text-gray-500"
+            disabled={props.isDisabled}
+        >
+            {props.children}
+        </BaseButton>
+    );
+}
--- src/renderer/page/game/overview.tsx
+++ src/renderer/page/game/overview.tsx
@@ -1,8 +1,26 @@
 import { useLocation } from "react-router";
 import { GamePropertiesDTO } from "@common/dto/game";
 import { ResourcesUtil } from "@renderer/util/resources";
+import Button from "@renderer/component/ui/button";
 
-interface State
+function ProfileCard()
+{
+    return (
+        <div className="flex gap-4 w-full p-4 bg-gray-800/30 border border-black/20 drop-shadow-lg drop-shadow-black">
+            <img
+                className="h-12"
+                src={ResourcesUtil.linkGameAsset("ZENLESS_ZONE_ZERO", "048617ceb68b40a45847078db347ba59.png")}
+                alt={"test"}
+            />
+            <div>
+                <h3 className="text-lg font-semibold">Test</h3>
+                <p className="text-sm text-gray-400">Test</p>
+            </div>
+        </div>
+    );
+}
+
+interface PageState
 {
     game: GamePropertiesDTO;
 }
@@ -10,7 +28,42 @@ interface State
 export default function GameOverviewPage()
 {
     const { state } = useLocation();
-    const { game } = state as State;
+    const { game } = state as PageState;
+
+    /**
+     * Temporary.
+     */
+    const installed = false;
+    const profiles = [
+        {
+            id: "1",
+            name: "Test"
+        },
+        {
+            id: "2",
+            name: "Test"
+        },
+        {
+            id: "3",
+            name: "Test"
+        },
+        {
+            id: "4",
+            name: "Test"
+        },
+        {
+            id: "5",
+            name: "Test"
+        },
+        {
+            id: "6",
+            name: "Test"
+        },
+        {
+            id: "7",
+            name: "Test"
+        }
+    ];
 
     if (game === undefined)
     {
@@ -27,15 +80,30 @@ export default function GameOverviewPage()
                 />
 
                 <img
-                    className="absolute bottom-10 left-10 max-w-60 max-h-60 drop-shadow-lg drop-shadow-black"
+                    className="absolute bottom-10 left-10 max-w-70 max-h-70 drop-shadow-lg drop-shadow-black"
                     src={ResourcesUtil.linkGameAsset(game.id, game.assets.logo)}
                     alt={game.details.name}
                 />
             </div>
-            <div className="flex gap-4 px-6 py-3">
-                <div className="font-light text-[15px] -space-y-0.5">
-                    <p>STATUS</p>
-                    <span>Not Installed</span>
+            <div className="px-6 pb-4 space-y-2">
+                <div className="flex gap-8 py-3">
+                    {installed && <Button>Create Profile</Button>}
+                    {!installed && <Button>Locate Installation</Button>}
+
+                    <div className="text-[15px] -space-y-0.5">
+                        <p>LAST PLAYED</p>
+                        <span className="font-light">None</span>
+                    </div>
+
+                    <div className="text-[15px] -space-y-0.5">
+                        <p>PLAY TIME</p>
+                        <span className="font-light">0 min</span>
+                    </div>
+                </div>
+                <div className="w-full flex flex-col gap-4 mt-6">
+                    {profiles.map((profile) => (
+                        <ProfileCard key={profile.id} />
+                    ))}
                 </div>
             </div>
         </main>
