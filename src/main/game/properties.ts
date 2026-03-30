984be42bfaf6b534c2a8d4484e9794c97cea654d
ZickZenni <zickzenniyt@gmail.com>
2026-03-30 13:18:53 +0200

    Improve the constructor for GameProperties for better readability

--- src/main/game/properties.ts
+++ src/main/game/properties.ts
@@ -8,13 +8,13 @@ export class GameProperties
 {
     private static readonly ENTRIES: GameProperties[] = [];
 
-    public static readonly STAR_RAIL = new GameProperties(
-        "STAR_RAIL",
-        {
+    public static readonly STAR_RAIL = new GameProperties({
+        id: "STAR_RAIL",
+        details: {
             name: "Honkai Star Rail",
             developer: GameDeveloper.HOYOVERSE
         },
-        {
+        installation: {
             executableFile: "StarRail.exe",
             requiredFiles: ["HoYoKProtect.sys", "mhypbase.dll", "StarRail_Data/"],
             locators: [
@@ -26,20 +26,21 @@ export class GameProperties
                 }
             ]
         },
-        {
+        assets: {
             icon: "ec01a34f7fc3b03448cc52f2a89d52e8.png",
             grid: "14219e4acfc4c50d323a47c2a6994299.png",
             hero: "33a4e204a72d69ec3786ff1cd02e7a66.png",
             logo: "804bfd285116c91c935176b2b199894d.png"
         }
-    );
-    public static readonly ZENLESS_ZONE_ZERO = new GameProperties(
-        "ZENLESS_ZONE_ZERO",
-        {
+    });
+    
+    public static readonly ZENLESS_ZONE_ZERO = new GameProperties({
+        id: "ZENLESS_ZONE_ZERO",
+        details: {
             name: "Zenless Zone Zero",
             developer: GameDeveloper.HOYOVERSE
         },
-        {
+        installation: {
             executableFile: "ZenlessZoneZero.exe",
             requiredFiles: ["HoYoKProtect.sys", "mhypbase.dll", "ZenlessZoneZero_Data/"],
             locators: [
@@ -51,30 +52,30 @@ export class GameProperties
                 }
             ]
         },
-        {
+        assets: {
             icon: "048617ceb68b40a45847078db347ba59.png",
             grid: "bf1e25110516b753b33dcc6d3266d71c.png",
             hero: "912c3958f7545cc891334c5f671c7555.png",
             logo: "6636876050dcade8ec8e3023b1afe9bc.png"
         }
-    );
+    });
 
     public readonly id: string;
     public readonly details: Readonly<GameProperties.Details>;
     public readonly installation: Readonly<GameProperties.Installation>;
     public readonly assets: Readonly<GameProperties.Assets>;
 
-    private constructor(
-        id: string,
-        details: GameProperties.Details,
-        installation: GameProperties.Installation,
-        assets: GameProperties.Assets
-    )
+    private constructor(data: {
+        id: string;
+        details: GameProperties.Details;
+        installation: GameProperties.Installation;
+        assets: GameProperties.Assets;
+    })
     {
-        this.id = id;
-        this.details = details;
-        this.installation = installation;
-        this.assets = assets;
+        this.id = data.id;
+        this.details = data.details;
+        this.installation = data.installation;
+        this.assets = data.assets;
 
         GameProperties.ENTRIES.push(this);
     }
