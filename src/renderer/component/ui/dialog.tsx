1ded35e679ce2597754253e1b79fb2b1b1d900da
ZickZenni <zickzenniyt@gmail.com>
2026-03-30 19:16:32 +0200

    Add backdrop blur effect to dialogs

--- src/renderer/component/ui/dialog.tsx
+++ src/renderer/component/ui/dialog.tsx
@@ -13,7 +13,7 @@ export function Dialog(props: DialogProps)
     return (
         <BaseDialog.Root open={props.open} onOpenChange={props.setOpen}>
             <BaseDialog.Portal>
-                <BaseDialog.Backdrop className="fixed inset-0 min-h-dvh bg-black opacity-50 data-ending-style:opacity-0 data-starting-style:opacity-0 supports-[-webkit-touch-callout:none]:absolute" />
+                <BaseDialog.Backdrop className="fixed inset-0 min-h-dvh bg-black/40 backdrop-blur-xs opacity-85 data-ending-style:opacity-0 data-starting-style:opacity-0 supports-[-webkit-touch-callout:none]:absolute" />
                 <BaseDialog.Popup className="fixed top-1/2 left-1/2 -mt-8 w-96 max-w-[calc(100vw-3rem)] -translate-x-1/2 -translate-y-1/2 bg-background-900 p-6 text-text border-1 border-border transition-all duration-150 data-ending-style:scale-95 data-ending-style:opacity-0 data-starting-style:scale-95 data-starting-style:opacity-0">
                     {props.children}
                 </BaseDialog.Popup>
